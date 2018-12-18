3dcde9e88872c499583867c367c8b0975c3a664e
Fernando Tavares <fernandotnl@hotmail.com>
2018-12-17 22:37:10 -0200

    Registering local components

--- src/Home.vue
+++ src/Home.vue
@@ -1,22 +1,15 @@
 <template>
   <div>
-    <p>Server Status: {{status}}</p>
-    <hr>
-    <button @click="changeStatus">Change Status</button>
+    <app-server-status :key="i" v-for="i in 5"></app-server-status>
   </div>
 </template>
 
 <script>
+import ServerStatus from './ServerStatus.vue';
+
 export default {
-  data() {
-    return {
-      status: 'Critical'
-    };
-  },
-  methods:{
-    changeStatus(){
-      this.status = this.status === 'Normal'? 'Critical' : 'Normal';
-    }
+  components: {
+    'app-server-status': ServerStatus
   }
 }
 </script>
