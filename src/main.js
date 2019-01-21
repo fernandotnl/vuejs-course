72a12900e2dfda29fc4f6a03098e3fe236e66544
Fernando Tavares <fernandotnl@hotmail.com>
2019-01-21 20:49:10 -0200

    Adding custom directive

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.directive('highlight', {
+  bind(el, binding, vnode){
+    el.style.backgroundColor = 'green';
+  }
+});
 
 new Vue({
   render: h => h(App),
