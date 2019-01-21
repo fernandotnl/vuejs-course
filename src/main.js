7aeeb276396ab25b32187ed37a20f10f829f3456
Fernando Tavares <fernandotnl@hotmail.com>
2019-01-21 20:55:25 -0200

    Adding modifier to custom directive

--- src/main.js
+++ src/main.js
@@ -4,12 +4,18 @@ import App from './App.vue'
 Vue.config.productionTip = false
 Vue.directive('highlight', {
   bind(el, binding, vnode){
-    if(binding.arg == 'background'){
-      el.style.backgroundColor = binding.value;
+    let delay = 0;
+    if(binding.modifiers['delayed']){
+      delay = 3000;
     }
-    else {
-      el.style.color = binding.value;
-    }
+    setTimeout(() => {
+      if(binding.arg == 'background'){
+        el.style.backgroundColor = binding.value;
+      }
+      else {
+        el.style.color = binding.value;
+      }
+    }, delay);   
   }
 });
 
