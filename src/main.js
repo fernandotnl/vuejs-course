9e5f22cd1c54a183f7951bfff52f10b8f00014ed
Fernando Tavares <fernandotnl@hotmail.com>
2019-01-21 20:53:26 -0200

    Passing arguments to custom directives

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import App from './App.vue'
 Vue.config.productionTip = false
 Vue.directive('highlight', {
   bind(el, binding, vnode){
-    //el.style.backgroundColor = 'green';
-    el.style.backgroundColor = binding.value;
+    if(binding.arg == 'background'){
+      el.style.backgroundColor = binding.value;
+    }
+    else {
+      el.style.color = binding.value;
+    }
   }
 });
 
