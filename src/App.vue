f1bb8ee3e217f680f30a6b737efd6226f9a557df
Fernando Tavares <fernandotnl@hotmail.com>
2019-01-21 20:19:37 -0200

    Built-in directives

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>Hello World!</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built-in Directives</h1>
+        <p v-text="'Some Text'"></p>
+        <p v-html="'<strong>Some strong text</strong>'"></p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
