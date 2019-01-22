0d0cfacb51e6d9b53bee1bfed21487ad40492076
Fernando Tavares <fernandotnl@hotmail.com>
2019-01-21 22:01:52 -0200

    Adding List component

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,15 @@
         <ul>
           <li :key="fruit" v-for="fruit in filteredFruits"> {{ fruit }}</li>
         </ul>
+        <hr>
+        <app-list></app-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import List from './List.vue';
 export default {
   data(){
     return {
@@ -34,6 +37,9 @@ export default {
         return element.match(this.filterText);
       })
     }
+  },
+  components:{
+    appList: List
   }
 }
 </script>
---
+++ src/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <hr>
+    <input v-model="filterText">
+    <ul>
+      <li :key="fruit" v-for="fruit in filteredFruits"> {{ fruit }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+      filterText: ''
+    };
+  },
+  computed: {
+    filteredFruits(){
+      return this.fruits.filter((element) => {
+        return element.match(this.filterText);
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
